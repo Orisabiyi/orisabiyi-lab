473f0cb7b0e09a3f7b1e8c9e54e813b0c3220850
orisabiyi <orisabiyidavid@gmail.com>
2025-11-01 07:30:15 +0100

    Add ExperienceSection component to display user skills

---
+++ components/exprience-section.tsx
@@ -0,0 +1,9 @@
+export default function ExperienceSection() {
+  return (
+    <section id="experience" className="flex flex-col items-center gap-10 py-10 w-full container mx-auto font-sora">
+      <h2 className="text-4xl font-medium">
+        My <span className="text-primary font-bold">Skills</span>
+      </h2>
+    </section>
+  );
+}
