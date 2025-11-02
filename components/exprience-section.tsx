aadc269a9e48e9478a5d4de967c35947f4420a35
orisabiyi <orisabiyidavid@gmail.com>
2025-11-02 08:42:58 +0100

    Update ExperienceSection with detailed job descriptions and render experience data dynamically

--- components/exprience-section.tsx
+++ components/exprience-section.tsx
@@ -5,7 +5,7 @@ const experienceData = [
     logo: '',
     duration: "Jan 2020 - Present",
     description:
-      "Developed and maintained the front end of the company's main product using React and TypeScript.",
+      "As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.",
   },
   {
     role: "Frontend Developer",
@@ -13,7 +13,7 @@ const experienceData = [
     logo: '',
     duration: "Jan 2020 - Present",
     description:
-      "Developed and maintained the front end of the company's main product using React and TypeScript.",
+      "As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.",
   },
   {
     role: "Frontend Developer",
@@ -21,7 +21,7 @@ const experienceData = [
     logo: '',
     duration: "Jan 2020 - Present",
     description:
-      "Developed and maintained the front end of the company's main product using React and TypeScript.",
+      "As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.",
   },
   {
     role: "Frontend Developer",
@@ -29,7 +29,7 @@ const experienceData = [
     logo: '',
     duration: "Jan 2020 - Present",
     description:
-      "Developed and maintained the front end of the company's main product using React and TypeScript.",
+      "As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.",
   },
 ];
 
@@ -37,11 +37,25 @@ export default function ExperienceSection() {
   return (
     <section
       id="experience"
-      className="flex flex-col items-center gap-10 py-10 w-full h-screen font-sora bg-gradient-to-b from-[var(--secondary)] via-[var(--primary-1)] to-[var(--secondary)]"
+      className="flex flex-col items-center gap-10 py-10 w-full font-sora bg-gradient-to-b from-[var(--secondary)] via-[var(--primary-1)] to-[var(--secondary)]"
     >
       <h2 className="text-4xl text-white">
         My <span className="font-bold">Experience</span>
       </h2>
+      <ul className="container mx-auto flex flex-col gap-6">
+        {
+          experienceData.map(({ role, company, duration, description }, index) => (
+            <li key={index} className="border border-white/20 p-6 text-white">
+              <div className="flex justify-between">
+                <h3 className="text-xl font-semibold">{role}</h3>
+                <p className="text-sm block">{duration}</p>
+              </div>
+              <h4 className="text-md">{company}</h4>
+              <p className="text-base mt-2">{description}</p>
+            </li>
+          ))
+        }
+      </ul>
     </section>
   );
 }
