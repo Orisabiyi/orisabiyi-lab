cf9c3e00e09554906494b968de223792436680ea
orisabiyi <orisabiyidavid@gmail.com>
2025-11-03 09:12:30 +0100

    Enhance ContactSection layout and styling; adjust spacing and improve readability

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -2,19 +2,50 @@ import Socials from "./socials";
 
 export default function ContactSection() {
   return (
-    <section className="flex items-center gap-10 py-10 container mx-auto font-sora">
+    <section className="flex items-center gap-20 py-10 container mx-auto font-sora">
       <form action="" className="w-1/3 flex flex-col gap-6">
-        <input type="text" placeholder="Your Name" className="w-full border-2 border-gray-500 px-4 py-2" />
-        <input type="email" placeholder="Email" className="w-full border-2 border-gray-500 px-4 py-2" />
-        <textarea placeholder="Your website (if exists)" className="w-full border-2 border-gray-500 px-4 py-2 resize-none h-32" />
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="w-full border-2 border-gray-500 px-4 py-2"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full border-2 border-gray-500 px-4 py-2"
+        />
+        <textarea
+          placeholder="Your website (if exists)"
+          className="w-full border-2 border-gray-500 px-4 py-2 resize-none h-32"
+        />
 
         <div className="flex items-stretch gap-5">
-          <button type="submit" className="w-1/2 bg-black text-white font-semibold">Get In Touch</button>
+          <button
+            type="submit"
+            className="w-1/2 bg-black text-white font-semibold">
+            Get In Touch
+          </button>
           <Socials />
         </div>
       </form>
 
-      <div className="flex-1"></div>
+      <div className="flex-1 space-y-4">
+        <h2 className="flex flex-col text-4xl font-bold">
+          <span>Let&apos;s <span className="text-primary">talk</span> for</span>
+          <span>Something <span className="text-primary">special</span></span>
+        </h2>
+        <p className="text-base w-4/5">
+          I care about creating experiences that feel good to use. Intuitive,
+          engaging and memorable. Whether it&apos;s through design, code or
+          experimentation, I&apos;m always looking for ways to make digital
+          interactions more meaningful and human centered
+        </p>
+
+        <div className="space-y-1">
+          <h3 className="text-2xl">orisabiyidavid@gmail.com</h3>
+          <p className="text-xl">+234 916 6331 550</p>
+        </div>
+      </div>
     </section>
   );
 }
