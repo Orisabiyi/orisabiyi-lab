19215a5edc3192c388ef1c20d8a70cb6391ab4fa
orisabiyi <orisabiyidavid@gmail.com>
2025-11-06 06:20:32 +0100

    feat: add bing site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export const metadata: Metadata = {
   description:
     "I'm Orisabiyi, a designer, software engineer and product thinker. Orisabiyi Lab is my space where I build things that matter, explore ideas that excite me and share the journey as it unfolds.",
   verification: {
-    google: "RW-SVzKPYwN0phkr-oAz6WaXcuDV542VlcQJWUD9AbI"
+    google: "RW-SVzKPYwN0phkr-oAz6WaXcuDV542VlcQJWUD9AbI",
+    other: {
+      "msvalidate.01": "3603F95AD54D3D426F263B58A813250C",
+    }
   },
   keywords: [
     "Software Engineer",
