5c1f5ce9e51760214501440d51f02a26cd62c518
orisabiyi <orisabiyidavid@gmail.com>
2025-11-17 11:45:39 +0100

    feat: enhance chat functionality with message logging and loading indicator

--- app/orisabiyi-ai/page.tsx
+++ app/orisabiyi-ai/page.tsx
@@ -2,27 +2,39 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { FaRegPenToSquare } from "react-icons/fa6";
 import { TbSend } from "react-icons/tb";
-import { useChat } from '@ai-sdk/react'
+import { UIMessage, useChat } from "@ai-sdk/react";
+import { FaStop } from "react-icons/fa";
 
 export default function Page() {
   const [input, setInput] = useState("");
-  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [chat, setChat] = useState<UIMessage[]>([]);
   const { messages, sendMessage } = useChat();
 
+  const lastMsg = messages[messages.length - 1];
+  const isLoading = lastMsg?.role === "user";
+
   useEffect(() => {
-    console.log(messages);
-  }, [messages])
+    console.log("Messages updated:", messages);
+    setChat(messages);
+  }, [messages]);
 
-  function handleMessageChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+  useEffect(() => {
+    console.log("Chat state updated:", chat);
+  }, [chat]);
+
+  function handleMessageChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
     setInput(e.target.value);
+  }
 
-    // auto-resize so wrapped lines remain visible (optional)
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "auto";
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+  function handleKeyDown(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(e as React.FormEvent);
     }
   }
 
@@ -57,35 +69,50 @@ export default function Page() {
       </div>
 
       <div className="p-10 h-full flex-1 text-black flex flex-col items-center justify-center gap-6">
-        <div className="flex items-center justify-center gap-4">
-          <figure className="w-16 h-16 bg-primary flex items-center justify-center p-2 rounded-full">
-            <Image
-              src="/images/orisabiyi-ai.svg"
-              alt="AI Bot"
-              width={100}
-              height={100}
-              className="w-full h-full block"
-            />
-          </figure>
-          <h1 className="text-xl font-medium">
-            Hey, I&apos;m Orisa, your product co-creator. Ready to shape your
-            next big idea?
-          </h1>
-        </div>
+        {chat.length === 0 && (
+          <>
+            <div className="flex items-center justify-center gap-4">
+              <figure className="w-16 h-16 bg-primary flex items-center justify-center p-2 rounded-full">
+                <Image
+                  src="/images/orisabiyi-ai.svg"
+                  alt="AI Bot"
+                  width={100}
+                  height={100}
+                  className="w-full h-full block"
+                />
+              </figure>
+              <h1 className="text-xl font-medium">
+                Hey, I&apos;m Orisa, your product co-creator. Ready to shape your
+                next big idea?
+              </h1>
+            </div>
 
-        <form className="relative w-2/3" onSubmit={handleSubmit}>
-          <textarea
-            ref={textareaRef}
-            value={input}
-            onChange={handleMessageChange}
-            placeholder="Type your message here..."
-            rows={1}
-            className="w-full border-2 border-primary rounded-full placeholder:text-primary placeholder:font-light py-2 pl-5 pr-12 !h-16 block outline-none resize-none overflow-x-hidden scrollbar-hide"
-          />
-          <button type="submit" className="absolute top-1/2 right-5 transform -translate-y-1/2 cursor-pointer">
-            <TbSend size={25} className="text-primary" />
-          </button>
-        </form>
+            <form className="relative w-2/3" onSubmit={handleSubmit}>
+              <textarea
+                value={input}
+                onChange={handleMessageChange}
+                onKeyDown={handleKeyDown}
+                placeholder="Type your message here..."
+                rows={1}
+                className="w-full border-2 border-primary rounded-full placeholder:align-middle placeholder:text-primary placeholder:font-light py-5 pl-5 pr-12 block outline-none resize-none overflow-x-hidden scrollbar-hide"
+              />
+              {!isLoading && (
+                <button
+                  type="submit"
+                  className="absolute top-1/2 right-5 transform -translate-y-1/2 cursor-pointer">
+                  <TbSend size={25} className="text-primary" />
+                </button>
+              )}
+              {isLoading && (
+                <button
+                  type="button"
+                  className="absolute top-1/2 right-5 transform -translate-y-1/2 cursor-pointer">
+                  <FaStop size={25} className="text-primary" />
+                </button>
+              )}
+            </form>
+          </>
+        )}
       </div>
     </section>
   );
