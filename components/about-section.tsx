bb158a1122ef08efb5b605bff1c54365f88c4126
orisabiyi <orisabiyidavid@gmail.com>
2025-11-24 07:59:26 +0100

    feat: enhance AboutSection styling with border and image transformations

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function AboutSection() {
   return (
     <section id="about-me" className="container mx-auto py-10 items-center flex flex-col md:flex-row gap-20 font-sora">
-      <figure className="px-10 md:p-0 md:w-1/2">
+      <figure className="px-10 md:p-0 md:w-1/2 border-2 border-primary rounded-2xl overflow-hidden">
         <Image
           src="/images/david-pic.png"
           // src="/images/me-icon.svg"
@@ -14,7 +14,7 @@ export default function AboutSection() {
           loading="lazy"
           fetchPriority="low"
           priority={false}
-          className="w-full h-auto about-img"
+          className="w-full h-auto scale-125 transform translate-y-8 about-img"
         />
       </figure>
       <div className="flex-1 flex flex-col gap-6 px-10 md:p-0">
