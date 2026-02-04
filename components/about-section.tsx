03fae0f4858f061805dcaaa874710acadf5a9e41
orisabiyi <orisabiyidavid@gmail.com>
2026-02-04 06:01:25 +0100

    feat: adjust layout and styling in AboutSection for improved visual consistency

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -3,11 +3,10 @@ import Image from "next/image";
 
 export default function AboutSection() {
   return (
-    <section id="about-me" className="container mx-auto py-32 items-center flex flex-col md:flex-row gap-20 font-sora">
-      <figure className="px-10 md:p-0 md:w-1/2 border-2 border-primary rounded-2xl overflow-hidden">
+    <section id="about-me" className="container mx-auto py-10 md:py-32 px-10 md:px-0 items-center flex flex-col md:flex-row gap-10 md:gap-20 font-sora">
+      <figure className="hidden md:block md:w-1/2 md:border-2 md:border-primary md:rounded-2xl overflow-hidden">
         <Image
           src="/images/david-pic.png"
-          // src="/images/me-icon.svg"
           alt="About Me"
           width={1200}
           height={600}
@@ -17,8 +16,8 @@ export default function AboutSection() {
           className="w-full h-auto scale-125 transform translate-y-8 about-img"
         />
       </figure>
-      <div className="flex-1 flex flex-col gap-6 px-10 md:p-0">
-        <h2 className="text-4xl">
+      <div className="flex-1 flex flex-col gap-6 md:p-0">
+        <h2 className="text-2xl md:text-4xl">
           <span>About</span>
           <span className="font-bold text-primary"> Me</span>
         </h2>
